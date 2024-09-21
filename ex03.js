e3b98d36a91b84f2ef0b1396fc2e21f5f05ba0d6
igorfernandescordeiro <ifcigor@gmail.com>
2024-09-21 11:50:13 -0300

    Adding more code for the api test

--- ex03.js
+++ ex03.js
@@ -1,24 +1,72 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { check, group, sleep } from "k6";
+import { faker } from 'https://esm.sh/@faker-js/faker';
 
 
-export default function() {
+export default function () {
 
+  let token;
+
+  group('Obtendo o token do usuario', () => {
     const bodyLogin = JSON.stringify({
-        usuarioLogin: 'cgts',
-        usuarioSenha: '123456'
-      });
-    
-      const opcoesLogin = {
-        headers: {
-          'Content-type': 'application/json'
-        }
-      };
+      usuarioLogin: 'cgts',
+      usuarioSenha: '123456'
+    });
+
+    const opcoesLogin = {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
 
 
 
     const responseLogin = http.post('http://165.227.93.41/lojinha/v2/login', bodyLogin, opcoesLogin);
-    sleep(1);
 
-    console.log(responseLogin);
+
+    check(responseLogin, {
+      'status code é 200': r => r.status === 200,
+      'validar mensagem de sucesso': r => r.json().message === 'Sucesso ao realizar o login'
+    })
+    token = responseLogin.json('data.token');
+  })
+
+
+  // ----------------------------------------------------------
+  group('Cadastrar um novo produto', () => {
+
+    // cadastrar um produto
+
+    const responseCadastroProduto = http.post(
+      'http://165.227.93.41/lojinha/v2/produtos',
+      JSON.stringify({
+        produtoNome: faker.food.fruit(),
+        produtoValor: 233,
+        produtoCores: [
+          "preto", "verde"
+        ]
+      }),
+      {
+        headers: {
+          'Content-type': 'application/json',
+          token: token
+        }
+      }
+    )
+    console.log(responseCadastroProduto.body)
+
+    check(responseCadastroProduto, {
+      'status code é 201': r => r.status === 201,
+      'validar mensagem de sucesso': r => r.json().message === 'Produto adicionado com sucesso',
+      'validar segunda cor do produto': r => r.json('data.produtoCores.1') === 'verde'
+    })
+
+    console.log(responseCadastroProduto.json('data.produtoCores.#'))
+  })
+
+  group('User Think Time após o cadastro', () => {
+
+    sleep(1);
+    
+  })
 }
