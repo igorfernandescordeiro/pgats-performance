7d16536039c3c686e5e1bba77a4aef34ce752f90
igorfernandescordeiro <ifcigor@gmail.com>
2024-10-27 19:19:12 -0300

    Adding a console log for study purpose and viewing in the github the console log

--- ex01.js
+++ ex01.js
@@ -2,6 +2,7 @@ import http from 'k6/http';
 import { sleep as userThinkerTime } from 'k6';
 import { numeroAleatorioAte } from './utils/numeros.js';
 
+console.log('EX01.js is RUNNING')
 export const options = {
   stages: [    
     {target: 5, duration: '5s'},
